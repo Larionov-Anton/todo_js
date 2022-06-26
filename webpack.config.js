ace48ed1617d1d48f788ec6105e966ea0f0c1028
Larionov-Anton <larionovanton05@gmail.com>
2022-06-26 15:30:26 +0300

    Create a basic file structure of the project

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'app.js',
+		publicPath: '/js'
+	}, 
+
+	devServer: {
+		static: {
+		  directory: path.join(__dirname, 'public'),
+		},
+		compress: true,
+		port: 9000,
+	 },
+
+	devtool: 'eval-cheap-source-map'
+};
+
