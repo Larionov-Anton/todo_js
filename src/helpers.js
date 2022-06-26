a29a11e6d20830885508dc2798f0a6d357cda645
Larionov-Anton <larionovanton05@gmail.com>
2022-06-26 16:22:18 +0300

    Create a data model

---
+++ src/helpers.js
@@ -0,0 +1,19 @@
+// Создать новый элемент дом дерева
+
+function createElement(tag, props, ...children) {
+	const element = document.createElement(tag);
+
+	Object.keys(props).forEach(key => element[key] = props[key]);
+
+	children.forEach(child => {
+		if (typeof child === 'string') {
+			child = document.createTextNode(child);
+		}
+
+		element.appendChild(child);
+	});
+
+	return element;
+}
+
+export {createElement};
