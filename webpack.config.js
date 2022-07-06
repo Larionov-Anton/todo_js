bd12074411605cdfb126b569f21db3a28d3d9b76
Larionov-Anton <larionovanton05@gmail.com>
2022-07-06 14:34:01 +0300

    Fix the error build production

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/index',
 	output: {
-		path: path.resolve(__dirname, 'public'),
+		path: path.resolve(__dirname, 'public/js/'),
 		filename: 'app.js',
 		publicPath: '/js'
 	}, 
